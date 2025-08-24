686f69b231a1887a28b5bb9f2571e0e794e99e20
mrzaizai2k <chibao24.12.1999@gmail.com>
2025-08-24 17:55:01 +0700

    add scripts

---
+++ script.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+source ~/anaconda3/etc/profile.d/conda.sh
+conda activate text2vid
+
+# Move into project root
+cd /home/mrzaizai2k/code_Bao/auto_tiktok
+
+# Run your app
+python src/app.py
